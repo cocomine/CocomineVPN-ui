4514d4dcb032b7a421285437af3bb65aa457d89b
cocomine <roguish.6888467468@gmail.com>
2024-08-27 01:28:06 +0800

    更換圖標

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {Download} from "./download";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {connectWebsocket} from "./websocks";
 
-const APP_VERSION = "1.11.7";
+const APP_VERSION = "1.11.8";
 
 // create router
 const router = createBrowserRouter([
